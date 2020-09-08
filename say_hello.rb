33da3cfd7b022f99627c6913454a01277b158a13
Alex Darvin <amdarvin14@gmail.com>
2020-09-08 18:58:14 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 def say_hello(name = "Ruby programmer", language = "Ruby")
-  puts "Hello, #{name}. We heard you are a great "
+  puts "Hello, #{name}. We heard you are a great #{language} programmer"
 end
 
 say_hello
